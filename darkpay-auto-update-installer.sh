ba30b3f7f0e08e3281d56b328876469acb947613
Skelt0r <s.kalter@orangepear.com>
2019-12-10 09:43:30 +0100

    Added auto updater and its installer

---
+++ darkpay-auto-update-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "Preparing the VPS for auto update setup"
+apt-get update >/dev/null 2>&1
+
+apt install -y curl >/dev/null 2>&1
+apt install -y jq >/dev/null 2>&1
+
+cd /root
+wget https://raw.githubusercontent.com/Skelt0r/darkpay-scripts/master/darkpay-auto-updater.sh
+chmod +x darkpay-auto-updater.sh
+
+line="1 0 * * * /root/darkpay-auto-updater.sh"
+(crontab -u userhere -l; echo "$line" ) | crontab -u userhere -
---
+++ darkpay-auto-updater.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+json=$(curl --silent -X GET  "https://raw.githubusercontent.com/DarkPayCoin/releases/master/clientBinaries.json" -H$
+latestVersion=$(echo $json | jq .clients | jq .darkpayd | jq .version | tr -d \")
+
+currentVersion=$(/usr/local/bin/darkpay-cli getnetworkinfo | grep subversion | cut -d : -f 3 | cut -d / -f 1)
+currentVersionShort=${currentVersion%.*}
+
+echo "latestVersion:"$latestVersion
+echo "currentVersion:"$currentVersion
+echo "currentVersionShort:"$currentVersionShort
+
+
+if [ "$latestVersion" = "$currentVersionShort"  -o "$latestVersion" = "$currentVersion" ]; then
+    echo "Already at latest version." #> /dev/null
+else
+   cd /root
+   service darkpay stop
+   fullpath=$(echo $json | jq .clients | jq .darkpayd | jq .platforms | jq .linux | jq .x64 | jq .download | jq .ur$
+   bin=$(echo $json | jq .clients | jq .darkpayd | jq .platforms | jq .linux | jq .x64 | jq .download | jq .bin | t$
+   filename=$(echo $fullpath | rev | cut -d \/ -f 1 | rev)
+   folder=$(echo $bin | cut -d \/ -f 1)
+   wget $fullpath
+   tar -xzvf $filename
+   rm /usr/local/bin/darkpayd
+   cp $bin /usr/local/bin/
+   service darkpay start
+   rm -R $folder
+   rm $filename
+fi
